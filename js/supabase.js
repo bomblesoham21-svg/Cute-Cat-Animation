03fdd249586bbcb5a6445bdd4532106c2490a171
DarkClown_94 <bomblesoham21@gmail.com>
2026-06-24 22:07:58 +0530

    Update supabase.js
    Signed-off-by: DarkClown_94 <bomblesoham21@gmail.com>

--- js/supabase.js
+++ js/supabase.js
@@ -3,3 +3,6 @@ window.supabase = supabase.createClient(
     SUPABASE_ANON_KEY
 );
 console.log("Supabase Connected");
+console.log("SUPABASE FILE LOADED");
+console.log(window.supabase);
+console.log(typeof window.supabase?.from);
