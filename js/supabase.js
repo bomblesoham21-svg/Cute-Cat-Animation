d341c571ad34b2dde73c4b77d62b43b25417af40
DarkClown_94 <bomblesoham21@gmail.com>
2026-06-24 21:50:00 +0530

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,8 +1,5 @@
-const supabaseClient = supabase.createClient(
+window.supabase = supabase.createClient(
     SUPABASE_URL,
     SUPABASE_ANON_KEY
 );
-
-window.supabase = supabaseClient;
-
 console.log("Supabase Connected");
