464222d86a9b7c240a0b68937da4bb858bc1694f
DarkClown_94 <bomblesoham21@gmail.com>
2026-06-24 21:25:04 +0530

    Update supabase.js
    Signed-off-by: DarkClown_94 <bomblesoham21@gmail.com>

--- js/supabase.js
+++ js/supabase.js
@@ -1,7 +1,8 @@
-const SUPABASE_URL = "https://lndeflbixwjxormrdmvg.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxuZGVmbGJpeHdqeG9ybXJkbXZnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODExODUxMTYsImV4cCI6MjA5Njc2MTExNn0.oIGrvENsT60tJ0aWQfYiSA6_cfiMAFJZxmKKTEa2sRA";
 const supabaseClient = supabase.createClient(
     SUPABASE_URL,
     SUPABASE_ANON_KEY
 );
+
+window.supabase = supabaseClient;
+
 console.log("Supabase Connected");
